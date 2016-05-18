fd182a3ce1c56017d25819adc94791a994ff5ab3
AndrewBresee <andrew.p.bresee@gmail.com>
2016-05-18 15:24:32 -0700

    Allow CORS on GET requests and display raw data

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 var app = angular.module('flickr-potato', []);
 
-app.controller('SearchPictures', function($scope) {
+
+app.controller('SearchPictures', function($scope, $http) {
 	$scope.querry = "Potato";
+	$scope.results; 
+
 	$scope.search = function() {
-		alert($scope.querry);
+
+		var url = 'https://api.flickr.com/services/feeds/photos_public.gne?tags='+$scope.querry+'&tagmode=all&format=json&jsoncallback=JSON_CALLBACK';
+
+		$http({
+		    method: 'JSONP',
+		    url: url
+		}).
+		success(function(status) {
+			console.log("WORKS!")
+			console.log(status);
+			$scope.results = status.items;
+		}).
+		error(function(status) {
+		  console.log("NO WORKS!")
+		});
+
+		
 	};
 });
